6d9c3bd6c4781b06195098690174067a357619c9
Ash Wilson <smashwilson@gmail.com>
2015-02-04 08:44:17 -0500

    Accept `-l` for selfsigned-keypair.

--- common/gen-selfsigned.sh
+++ common/gen-selfsigned.sh
@@ -4,13 +4,15 @@
 
 # Arguments:
 # - NAME
+# - LIFETIME in days
 generate_selfsigned()
 {
   local NAME=$1
+  local LIFETIME=$2
 
   info "Generating a self-signed keypair for: <${NAME}>"
 
-  openssl req -x509 -newkey rsa:2048 -days 365 -nodes -batch \
+  openssl req -x509 -newkey rsa:2048 -days ${LIFETIME} -nodes -batch \
     -keyout /certificates/${NAME}-key.pem \
     -out /certificates/${NAME}-cert.pem
 
